64d68108896bf508c7bf99f0690fbd3e26415e44
Courtney Schild <courtneyschild@gmail.com>
2020-01-31 16:35:28 -0800

    add method to seed database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_breweries
+  end
+
+  def generate_breweries
+    10.times do |i|
+      brewery = Brewery.create!(
+        name: Faker::Beer.brand,
+        street_address: Faker::Address.street_address,
+        city: Faker::Address.city,
+        state: Faker::Address.state,
+        zip: Faker::Address.zip,
+        phone_number: Faker::PhoneNumber.phone_number,
+        website: Faker::Internet.url,
+        neighborhood: Faker::Compass.ordinal_abbreviation,
+        pet_friendly: Faker::Boolean.boolean,
+      )
+      puts "#{i}. #{brewery.name}, #{brewery.id}"
+    end
+  end
+
+end
+
+Seed.begin
